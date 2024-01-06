ae9eee822c2141349b528e706278c31c4a2adcf6
unknown <azizbekabdunabiyev98@gmail.com>
2024-01-06 12:28:56 +0500

    addd

---
+++ app/projects/[id]/not-found.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundSingleProject = () => {
+  return (
+    <div className="container">
+      <h1>project is not found</h1>
+    </div>
+  );
+};
+
+export default NotFoundSingleProject;
